47650efdc3197cbffa4765485d0fc6d85703befe
tangqi <tangqi_1987@sina.com>
2018-04-10 16:49:54 +0800

    修改文件上传存储方式 路径 去掉qiniu 相关代码

--- src/Uploader/UploadFile.php
+++ src/Uploader/UploadFile.php
@@ -11,74 +11,58 @@ use iBrand\UEditor\Uploader\Upload;
  *
  * @package iBrand\UEditor\Uploader
  */
-class UploadFile  extends Upload{
-    use UploadQiniu;
-    public function doUpload()
-    {
+class UploadFile extends Upload
+{
+	public function doUpload()
+	{
 
 
-        $file = $this->request->file($this->fileField);
-        if (empty($file)) {
-            $this->stateInfo = $this->getStateInfo("ERROR_FILE_NOT_FOUND");
-            return false;
-        }
-        if (!$file->isValid()) {
-            $this->stateInfo = $this->getStateInfo($file->getError());
-            return false;
+		$file = $this->request->file($this->fileField);
+		if (empty($file)) {
+			$this->stateInfo = $this->getStateInfo("ERROR_FILE_NOT_FOUND");
 
-        }
+			return false;
+		}
+		if (!$file->isValid()) {
+			$this->stateInfo = $this->getStateInfo($file->getError());
 
-        $this->file = $file;
+			return false;
+		}
 
-        $this->oriName = $this->file->getClientOriginalName();
+		$this->file = $file;
 
-        $this->fileSize = $this->file->getSize();
-        $this->fileType = $this->getFileExt();
+		$this->oriName = $this->file->getClientOriginalName();
 
-        $this->fullName = $this->getFullName();
+		$this->fileSize = $this->file->getSize();
+		$this->fileType = $this->getFileExt();
+		$this->fullName = $this->getFullName();
+		$this->filePath = $this->getFilePath();
 
+		//检查文件大小是否超出限制
+		if (!$this->checkSize()) {
+			$this->stateInfo = $this->getStateInfo("ERROR_SIZE_EXCEED");
 
-        $this->filePath = $this->getFilePath();
+			return false;
+		}
+		//检查是否不允许的文件格式
+		if (!$this->checkType()) {
+			$this->stateInfo = $this->getStateInfo("ERROR_TYPE_NOT_ALLOWED");
 
-        $this->fileName = basename($this->filePath);
+			return false;
+		}
 
+		if (config('UEditorUpload.core.mode') == 'local') {
+			try {
+				$this->file->storeAs($this->filePath, $this->fullName, 'public');
 
-        //检查文件大小是否超出限制
-        if (!$this->checkSize()) {
-            $this->stateInfo = $this->getStateInfo("ERROR_SIZE_EXCEED");
-            return false;
-        }
-        //检查是否不允许的文件格式
-        if (!$this->checkType()) {
-            $this->stateInfo = $this->getStateInfo("ERROR_TYPE_NOT_ALLOWED");
-            return false;
-        }
+				$this->stateInfo = $this->stateMap[0];
+			} catch (\Exception $exception) {
+				$this->stateInfo = $this->getStateInfo("ERROR_WRITE_CONTENT");
 
-        if(config('UEditorUpload.core.mode')=='local'){
-            try {
-                $this->file->move(dirname($this->filePath), $this->fileName);
+				return false;
+			}
+		}
 
-                $this->stateInfo = $this->stateMap[0];
-
-            } catch (FileException $exception) {
-                $this->stateInfo = $this->getStateInfo("ERROR_WRITE_CONTENT");
-                return false;
-            }
-
-        }else if(config('UEditorUpload.core.mode')=='qiniu'){
-
-            $content=file_get_contents($this->file->getPathname());
-            return $this->uploadQiniu($this->filePath,$content);
-
-        }else{
-            $this->stateInfo = $this->getStateInfo("ERROR_UNKNOWN_MODE");
-            return false;
-        }
-
-
-
-
-        return true;
-
-    }
+		return true;
+	}
 }
